a2ec99390d383cfd2cf62afad57a27a76b0761e9
Christian Diaz <contacto.chd@gmail.com>
2020-05-06 20:24:19 -0400

    Proyecto finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
 // Método que espera que se cargue todo el documento HTML, antes de intervenirlo
 $(function () {
+  //función de smooth scroll
+  $("a").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(gato).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = gato;
+        }
+      );
+    }
+  });
+
   // JS de tooltip Bootstrap 4.4
   $('[data-toggle="tooltip"]').tooltip();
 });
